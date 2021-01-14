d20bfe177344ff9c895b2e0ec5e04a7f34de3410
Stefania <estefansilvi@gmail.com>
2021-01-14 19:39:26 +0100

    js navbar dropdown menù completo e funzionante

--- js/script.js
+++ js/script.js
@@ -5,3 +5,28 @@ $('.title-3').mouseenter(function(){
 $('.title-3').mouseleave(function(){
   $('.feauture').removeClass('display-block')
 })
+
+$('.title-4').mouseenter(function(){
+  $('.plans').addClass('display-block')
+})
+
+$('.title-4').mouseleave(function(){
+  $('.plans').removeClass('display-block')
+})
+
+$('.title-5').mouseenter(function(){
+  $('.about-as').addClass('display-block')
+})
+
+$('.title-5').mouseleave(function(){
+  $('.about-as').removeClass('display-block')
+})
+
+
+$('.title-6').mouseenter(function(){
+  $('.help').addClass('display-block')
+})
+
+$('.title-6').mouseleave(function(){
+  $('.help').removeClass('display-block')
+})
